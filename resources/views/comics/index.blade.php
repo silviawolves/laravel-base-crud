d18a22aaf6d8a0e8a3273fd91ab7f6ee0fadc66b
silviawolves <silviacanepa.w@gmail.com>
2022-08-02 17:15:04 +0200

    ripreso però vabbè -

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,13 +2,26 @@
 
 @section('page_content')
 
-    <h2>Fumetti</h2>
-    <p>una pagina con dei fumetti</p>
+    <h2 class="py-3">Fumetti</h2>
+    <p>Una pagina con dei fumetti</p>
 
+    <a href="-" class="btn btn-danger mb-4">Add Comic</a>
+    
+    <div class="row">
     @foreach ($comics as $comic)
 
-        <h6>{{ $comic['title'] }}</h6>
-        
+        <div class="col-2">
+            <div class="card" style="width: 100%; text-align: center;">
+                <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">{{ $comic['title'] }}</h5>
+                <h6 class="card-text" style="font-size: .8rem;">{{ $comic['series'] }}</h6>
+                <a href="-" class="btn btn-success">Details</a>
+                </div>
+            </div>
+        </div>
+
     @endforeach
+    </div>
 
 @endsection
