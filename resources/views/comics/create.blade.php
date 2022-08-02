d4c9f68d9e731eb94d5175f10694868227281e59
silviawolves <silviacanepa.w@gmail.com>
2022-08-02 17:55:13 +0200

    form con btn

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -3,37 +3,38 @@
 @section('page_content')
 
     <h2 class="py-3">Aggiungi il tuo nuovo fumetto</h2>
-    <p>yea!</p>
+    <p>yea che bello, non vedevo l'ora!</p>
 
     <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label text-uppercase">Comic Title</label>
-        <input type="text" class="form-control" id="exampleFormControlInput1">
+        <input type="text" class="form-control" name="title" id="exampleFormControlInput1">
     </div>
     <div class="mb-3">
         <label for="exampleFormControlTextarea1" class="form-label text-uppercase">Comic Description</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <textarea class="form-control" name="description" id="exampleFormControlTextarea1" rows="3"></textarea>
     </div>
     <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label text-uppercase">Comic Image</label>
-        <input type="text" class="form-control" id="exampleFormControlInput1">
+        <input type="text" class="form-control" name="thumb" id="exampleFormControlInput1">
     </div>
     <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label text-uppercase">Comic price</label>
-        <input type="number" class="form-control" id="exampleFormControlInput1">
+        <input type="number" class="form-control" name="price" id="exampleFormControlInput1">
     </div>
     <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label text-uppercase">Comic series</label>
-        <input type="text" class="form-control" id="exampleFormControlInput1">
+        <input type="text" class="form-control" name="series" id="exampleFormControlInput1">
     </div>
     <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label text-uppercase">Comic Sale Date</label>
-        <input type="date" class="form-control" id="exampleFormControlInput1">
+        <input type="date" class="form-control" name="sale_date" id="exampleFormControlInput1">
     </div>
     <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label text-uppercase">Comic Type</label>
-        <input type="text" class="form-control" id="exampleFormControlInput1">
+        <input type="text" class="form-control" name="type" id="exampleFormControlInput1">
     </div>
 
     <button type="submit" class="btn btn-success mb-5">Aggiungi fumetto</button>
+    <a href="{{ route('home') }}" class="btn btn-secondary mb-5">Annulla</a>
 
 @endsection
